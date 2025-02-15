4fea859c5f8eeea6b672deb4d29f961f8d602c15
Essoufi-oussama <87605323+Essoufi-oussama@users.noreply.github.com>
2025-02-15 21:59:41 +0100

    fixed small issue in expanding but withh no 25 lines to be done

--- parsing/expanding.c
+++ parsing/expanding.c
@@ -6,40 +6,52 @@
 /*   By: oessoufi <oessoufi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 16:19:20 by oessoufi          #+#    #+#             */
-/*   Updated: 2025/02/15 21:10:39 by oessoufi         ###   ########.fr       */
+/*   Updated: 2025/02/15 21:57:03 by oessoufi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-char	*build_expanded_string(char *token, char *previous, int i)
+char    *build_expanded_string(char *token, char *previous, int i)
 {
-	char	*next;
-	char	*expanded;
-	int		j;
-	int		k;
+    char    *next;
+    char    *expanded;
+    int        j;
+    int        k;
 
-	j = 0;
-	k = 0;
-	while(token[i + j] && token[i + j] != '$')
-		j++;
-	expanded = malloc(sizeof(char) * (j + 1));
-	ft_strlcpy(expanded, token + i, j + 1);
-	if (getenv(expanded))
-		expanded = ft_strdup(getenv(expanded));
-	else
-		i++;
-	previous = malloc(sizeof(char) * (i));
-	ft_strlcpy(previous, token, i);
-	while(token[i + j + k])
-		k++;
-	next = malloc(sizeof(char) *(k + 1));
-	ft_strlcpy(next, token + i + j, k + 1);
-	token = ft_strjoin(previous, expanded);
-	token = ft_strjoin(token, next);
-	return(token);
+    j = 0;
+    k = 0;
+    if (token[i] == '?')
+    {
+        expanded = ft_strdup("0");// ghanchanjiha lakhir status code kin3rf kindir
+        j++;
+    }
+    else
+    {
+        while(token[i + j] && token[i + j] != '$')
+            j++;
+        expanded = malloc(sizeof(char) * (j + 1));
+        ft_strlcpy(expanded, token + i, j + 1);
+        if (getenv(expanded))
+		{
+            expanded = ft_strdup(getenv(expanded));
+    		previous = malloc(sizeof(char) * (i));
+    		ft_strlcpy(previous, token, i);
+		}
+        else
+		{
+           	previous = malloc(sizeof(char) * (i + 1));
+    		ft_strlcpy(previous, token, i + 1);
+		}
+    }
+    while(token[i + j + k])
+        k++;
+    next = malloc(sizeof(char) *(k + 1));
+    ft_strlcpy(next, token + i + j, k + 1);
+    token = ft_strjoin(previous, expanded);
+    token = ft_strjoin(token, next);
+    return(token);
 }
-
 char	*expand_token(char *token)
 {
 	char	*previous;
